c642dd189b54c2a916ed601a6def68bc6abfc6d3
Henry Kredatus <hmkredatus@liberty.edu>
2026-01-26 00:05:51 -0500

    Lab 1 Upload

---
+++ asgn01-Test Upload to Server/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<header>
+    <title>Assignment 01</title>
+</header>
+<body>
+    <h1>Assignment 01</h1>
+    <p>This is a webpage using PHP</p>
+    <br />
+
+    <?php
+        echo "Hello World!<br />";
+        echo "My name is Henry Kredatus";
+    ?>
+  </body>
+</html>
